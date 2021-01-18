d7530dba440d5410f46809ce164bcb8309a0d8b1
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-01-18 12:46:54 +0000

    integrates downloads

--- app/service.py
+++ app/service.py
@@ -10,7 +10,6 @@ from datetime import datetime, timedelta
 from os.path import abspath, join as join_path, pardir
 from typing import Union
 from functools import lru_cache
-from asyncio import get_event_loop
 
 # 3rd party:
 from flask import Flask, Response, g, render_template, make_response, request
@@ -26,10 +25,12 @@ from opencensus.trace import config_integration
 from opencensus.trace.propagation.trace_context_http_header_format import TraceContextPropagator
 
 # Internal:
-from app.easy_read.views import easy_read, local_easy_read
+from app.easy_read import (
+    easy_read, local_easy_read, get_validated_postcode,
+    create_and_redirect as get_pdf
+)
 from app.common.caching import cache_client
 from app.common.exceptions import HandledException
-# from app.database import CosmosDB, Collection
 from app.common.utils import add_cloud_role_name
 from app.storage import StorageClient
 from app.database import CosmosDB, Collection
@@ -52,6 +53,7 @@ LATEST_PUBLISHED_TIMESTAMP = {
     "path": "info/latest_published"
 }
 NOT_AVAILABLE = "N/A"
+PDF_TYPE = "application/pdf"
 INSTRUMENTATION_CODE = getenv("APPINSIGHTS_INSTRUMENTATIONKEY", "")
 AI_INSTRUMENTATION_KEY = f"InstrumentationKey={INSTRUMENTATION_CODE}"
 SERVER_LOCATION_KEY = "SERVER_LOCATION"
@@ -60,7 +62,7 @@ PYTHON_TIMESTAMP_LEN = 24
 HTTP_DATE_FORMAT = "%a, %d %b %Y %H:%M:%S GMT"
 LOG_LEVEL = getenv("LOG_LEVEL", "INFO")
 
-timestamp_pattern = "%A %d %B %Y at %I:%M %p"
+timestamp_pattern = "%A %d %B %Y at %-I:%M %p"
 timezone_LN = timezone("Europe/London")
 
 lookup_db = CosmosDB(Collection.LOOKUP)
@@ -115,9 +117,12 @@ def format_timestamp(latest_timestamp: str) -> str:
 
 @app.template_filter()
 @lru_cache(maxsize=64)
-def as_datestamp(latest_timestamp: str) -> str:
-    datestamp = latest_timestamp.split("T")[0]
-    return datestamp
+def as_date(latest_timestamp: str) -> str:
+    ts_python_iso = latest_timestamp[:-1] + "+00:00"
+    ts = datetime.fromisoformat(ts_python_iso)
+    ts_london = ts.astimezone(timezone_LN)
+    formatted = ts_london.strftime("%A, %d %B %Y")
+    return formatted
 
 
 @app.template_filter()
@@ -182,7 +187,7 @@ def handle_404(err):
         return err
 
     app.logger.info(f"404 - Not found", extra={'custom_dimensions': {"url": request.url}})
-    return render_template("errors/404.html"), 404
+    return render_template("html/errors/404.html"), 404
 
 
 @app.errorhandler(Exception)
@@ -208,7 +213,7 @@ def handle_500(err):
 
     app.logger.exception(err, extra={'custom_dimensions': additional_info})
 
-    return render_template("errors/500.html"), 500
+    return render_template("html/errors/500.html"), 500
 
 
 @cache_client.memoize(timeout=120)
@@ -234,12 +239,13 @@ def inject_globals():
 def prep_service():
     exporter = AzureExporter(connection_string=AI_INSTRUMENTATION_KEY)
     exporter.add_telemetry_processor(add_cloud_role_name)
+    propagator = TraceContextPropagator()
 
     _ = FlaskMiddleware(
         app=app,
         exporter=exporter,
         sampler=AlwaysOnSampler(),
-        propagator=TraceContextPropagator()
+        propagator=propagator
     )
 
     handler = AzureLogHandler(connection_string=AI_INSTRUMENTATION_KEY)
@@ -280,18 +286,26 @@ def prepare_response(resp: Response):
         "%Y-%m-%dT%H:%M:%S.%fZ"
     )
 
-    expires = datetime.now() + timedelta(minutes=1, seconds=30)
-
-    resp.headers['Last-Modified'] = last_modified.strftime(HTTP_DATE_FORMAT)
-    resp.headers['Expires'] = expires.strftime(HTTP_DATE_FORMAT)
+    resp.last_modified = last_modified
+    resp.expires = datetime.now() + timedelta(minutes=1, seconds=30)
+    resp.cache_control.max_age = 30
+    resp.cache_control.public = True
+    resp.cache_control.s_maxage = 90
+    resp.cache_control.must_revalidate = True
     resp.headers['PHE-Server-Loc'] = SERVER_LOCATION
 
-    try:
-        minified = [minifier.get_minified(item.decode(), 'html') for item in resp.response]
-        data = str.join("", minified).encode()
-        resp.set_data(data)
-    except UnicodeDecodeError as e:
-        app.logger.warning(e)
+    resp.add_etag()
+
+    if resp.content_type == PDF_TYPE:
+        try:
+            minified = [
+                minifier.get_minified(item.decode(), 'html')
+                for item in resp.response
+            ]
+            data = str.join("", minified).encode()
+            resp.set_data(data)
+        except UnicodeDecodeError as e:
+            app.logger.warning(e)
 
     return resp
 
@@ -306,20 +320,26 @@ def health_check(**kwargs):
     raise RuntimeError("Healthcheck failed.")
 
 
-@app.route("/easy_read", methods=("OPTIONS", "GET"))
-def main_responder(**kwargs):
-    if request.method == "GET":
-        return easy_read()
-
-    return app.make_default_options_response()
-
-
-@app.route("/easy_read/<area_type>/<area_code>", methods=("OPTIONS", "GET"))
+@app.route("/easy_read/", methods=("HEAD", "OPTIONS", "GET"))
+@app.route("/easy_read/<area_type>/<area_code>", methods=("HEAD", "OPTIONS", "GET"))
 def local_responder(area_type, area_code, **kwargs):
     if request.method == "GET":
-        return local_easy_read(area_type, area_code)
+        postcode = get_validated_postcode(request.args)
+
+        if request.method == "GET":
+            return easy_read(g.timestamp, postcode)
+
+        return local_easy_read(g.timestamp, area_type, area_code)
 
     return app.make_default_options_response()
+
+
+@app.route("/easy_read/download/", methods=("HEAD", "OPTIONS", "GET"),
+           defaults={"area_type": None, "area_code": None})
+@app.route("/easy_read/download/<area_type>/<area_code>",
+           methods=("HEAD", "OPTIONS", "GET"))
+def as_pdf(area_type, area_code):
+    return get_pdf(area_type, area_code)
 
 
 if __name__ == "__main__":
