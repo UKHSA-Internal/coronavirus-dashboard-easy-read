2abe97b889bfeeaf502b9ffe861dfb300247c1f3
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-01-19 00:24:18 +0000

    adds server name

--- app/config.py
+++ app/config.py
@@ -8,8 +8,12 @@ from os import getenv
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
+IS_DEV = getenv("IS_DEV", "0")
+
+
 class Config(object):
     TESTING = getenv("IS_DEV", "0") == "1"
-    DEBUG = getenv("IS_DEV", "0") == "1"
+    DEBUG = IS_DEV
     SECRET_KEY = getenv('FLASK_SECRET_KEY')
     APPLICATION_ROOT = "/"
+    SERVER_NAME = getenv("URL_LOCATION", None) if not IS_DEV else None
