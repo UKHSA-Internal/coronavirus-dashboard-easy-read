063866e9d038eb0ab466ea93ad8acf5bd65c22a1
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-03-29 00:46:50 +0100

    changes path to nginx conf file

--- prestart.py
+++ prestart.py
@@ -5,11 +5,11 @@ from os import getenv
 
 location = getenv("URL_LOCATION")
 
-with open('/etc/nginx/conf.d/engine.conf', 'r') as fp:
+with open('/opt/hosts.nginx', 'r') as fp:
     config = fp.read()
 
 
 config = config.replace("${URL_LOCATION}", location)
 
-with open('/etc/nginx/conf.d/engine.conf', 'w') as fp:
+with open('/opt/hosts.nginx', 'w') as fp:
     print(config, file=fp)
